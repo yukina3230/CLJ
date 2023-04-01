73085fa5ea5838f329b4d7733f41ed4bbc765500
yukina3230 <thienphuc3230@gmail.com>
2023-04-01 07:58:56 +0700

    commit

---
+++ CLJ/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace CLJ.Models;
+
+public abstract class Product
+{
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+}
---
+++ CLJ/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace CLJ;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
